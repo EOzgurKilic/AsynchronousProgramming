a87710b4fde85f24b79bdd24445dc3333674e254
Kilic <v-efe.kilic@lionbridge.com>
2026-09-15 17:56:50 +0200

    Blocking and Thread Synchronisation tutorial is finished

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -127,7 +127,7 @@ class Program
         #region Locking
 
         /*int i = 0;
-        object locker = new object();
+        Lock locker = new();
         Thread thread1 = new(() =>
         {
             lock (locker)
@@ -221,5 +221,170 @@ class Program
         thread.Interrupt(); //Wakes the worker by throwing the exception above.
         thread.Join();*/
         #endregion
+
+
+
+        //----------------------------------------------------------------------------------
+        //Thread Synchronization & Blocking Synchronization
+
+            #region Your spinning practice (kept as a comment)
+            /*
+            // utilized approach to keep multithreads in sync that make operations on the same source (variables, etc.) while running at the same time
+            bool thread1Access = true;
+            bool thread2Access = false;
+            bool ending = false;
+            int num1 = 0;
+            Thread thread1 = new(() =>
+            {
+                int count = 0;
+                while (count++ < 30)
+                {
+                    Thread.Sleep(1000);
+                    if (thread1Access)
+                    {
+                        if (num1 < 10)
+                        {
+                            System.Console.WriteLine(num1++);
+                        }
+                        else
+                        {
+                            thread1Access = false;
+                            thread2Access = true;
+                        }
+                    }
+                }
+                ending = true;
+            });
+            Thread thread2 = new(() =>
+            {
+                while (true)
+                {
+                    if (thread2Access)
+                    {
+                        num1 /= 10;
+                        thread2Access = false;
+                        thread1Access = true;
+                    }
+                    if (ending) break;
+                }
+            });
+            thread1.Start();
+            thread2.Start();
+            */
+            #endregion
+            #region Minimal spinning advanced practice
+            // Spinning fits a very short wait: the consumer waits for the producer's result.
+            /*int result = 0;
+            bool resultIsReady = false;
+
+            Thread producer = new(() =>
+            {
+                result = 42;
+                Volatile.Write(ref resultIsReady, true);
+            });
+
+            producer.Start();
+            SpinWait.SpinUntil(() => Volatile.Read(ref resultIsReady));
+
+            Console.WriteLine($"Result: {result}");
+            producer.Join();*/
+            #endregion
+
+
+
+
+            #region Monitor.Enter, Monitor.TryEnter and Monitor.Exit Methods for Locking and LockTaken to ensure the Locking action
+            //They basically represent the functional method version of the locking mechanism.
+            //Enter locks and exit unlocks the specified object
+
+            // Use lock for ordinary protection of shared data.
+            // Use Monitor directly when you need Monitor.Wait, Monitor.Pulse,
+            // or Monitor.TryEnter with a timeout.
+
+            // Monitor.Exit should be placed in finally so the monitor is released even if an exception occurs;
+            // lock does this automatically, so it does not need an explicit try-finally block.
+
+            /*int i = 0;
+            Lock lockObj = new();
+            Thread thread1 = new(() =>
+            {
+                try{
+                    Monitor.Enter(lockObj);
+                    for (i = 0; i < 10; i++)
+                    {
+                        System.Console.WriteLine($"Thread 1 {i}");
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lockObj);
+                }
+            });
+            Thread thread2 = new(() =>
+            {
+                
+                try{
+                    Monitor.Enter(lockObj);
+                    for (i = 0; i < 10; i++)
+                    {
+                        System.Console.WriteLine($"Thread 2 {i}");
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lockObj);
+                }
+            });
+            thread1.Start();
+            thread2.Start();*/
+
+
+            //LockTaken
+            /*Lock lockObj = new();
+            Thread thread1 = new(() =>
+            {
+                
+                try{
+                    bool lockTaken = false;
+                    Monitor.Enter(lockObj,ref lockTaken);
+                    if(lockTaken){    
+                        for (int i = 0; i < 10; i++)
+                        {
+                            System.Console.WriteLine($"Thread 1 {i}");
+                        }
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lockObj);
+                }
+            });
+            thread1.Start();*/
+
+
+
+
+            //Monitor.TryEnter
+            //We get a bool if the action is taken or not.
+            Lock lockObj = new();
+            Thread thread1 = new(() =>
+            {
+                
+                var result = Monitor.TryEnter(lockObj, 0);
+                if (result)
+                {
+                    try
+                    {
+                        for(int i = 0; i < 15; i++)
+                            System.Console.WriteLine(i);
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lockObj);
+                    }
+                }
+            });
+            thread1.Start();
+            #endregion
     }
 }
